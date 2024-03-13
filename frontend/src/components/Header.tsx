81bc628c489b6d41eee5a9de89770d62ecc91dab
PedroHSBernardes <133461209+PedroHenriqueSBer@users.noreply.github.com>
2024-03-13 20:09:39 -0300

    Template

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,8 @@
+
+export const Header = () => {
+    return (
+        <header className="w-full p-4 bg-slate-100 shadow flex items-center">
+            <h1 className="font-semibold text-slate-900">Pedro Henrique de Sousa Bernardes</h1>
+        </header>
+    )
+}
