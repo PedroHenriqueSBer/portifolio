55d793bebe8bc96ce3c55fde93dba1ca1637474e
PedroHSBernardes <133461209+PedroHenriqueSBer@users.noreply.github.com>
2024-03-13 23:43:08 -0300

    pdf

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Github } from "lucide-react"
+import { FileText, Github } from "lucide-react"
 
 export const Header = () => {
   return (
@@ -6,13 +6,16 @@ export const Header = () => {
       <div>
         <h1 className="font-semibold text-slate-900">Pedro Henrique de Sousa Bernardes</h1>
       </div>
-      <div>
+      <div className="flex gap-2">
         <div className="hover:bg-slate-200 p-[0.5rem] rounded-full">
           <a target="_blank" href="https://github.com/PedroHenriqueSBer" className="w-full h-full">
             <Github />
           </a>
         </div>
-
+        <a target="_blank" href="" className="flex gap-2 items-center bg-slate-800 text-slate-200 rounded px-2 hover:bg-slate-700">
+          <FileText />
+          Currículo
+        </a>
       </div>
     </header>
   )
